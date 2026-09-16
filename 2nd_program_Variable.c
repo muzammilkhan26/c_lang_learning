0fcd7fc236524f5965cb85bada8e1f2172c40b94
error <error@error.error>
2026-09-16 23:19:48 +0500

    2nd Promgram of Veriables

---
+++ 2nd_program_Variable.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    /* 
+    is main int ye bata raha hai k Varibale integer ho ga
+    or a Varible hai or us main 1 ko store kiya gaya hai
+    Varibale ki abhi ham 3 types parh rahy hain 
+    int char or float
+    int yani us main 1 2 3 4 5 6 7 8 9 0 tak sari values ho gi yani k bas numbers
+    chat matlab us main sirf character ho gay A to Z
+    float yani us main numbers decimal points k sath ho gay jese 1.0 
+    */
+    int a;
+    a = 1;
+    printf("a Value is %d", a);
+    printf("\n");
+
+    char b;
+    b = 'M';
+    printf("b Value is %c", b);
+    printf("\n");
+
+    float c;
+    c = 1.5;
+    printf("c Value is %f", c);
+
+    return 0;
+}
